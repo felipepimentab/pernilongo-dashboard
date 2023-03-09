df860fa376384a8fc542855c932bbcf0cb501812
Felipe Pimenta <felipe.pbernardo@gmail.com>
2023-03-09 20:11:58 -0300

    adiciona demo mqtt vue3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <!-- eslint-disable no-undef -->
 <script setup lang="ts">
+import mqtt from 'mqtt';
+
 var texto = 'Não recebi nada ainda.';
+
+const client = mqtt.connect('ws://54.234.76.162:9001', {
+  username: "pipopb",
+  password: "pipopb109",
+});
+client.subscribe('mqtt/dht');
+client.on('message', function (topic, payload) {
+  if(payload)console.log(payload);
+});
 </script>
 
 <template>
